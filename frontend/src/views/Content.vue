3806536ea18ba9eee00907f30a188f33ebbab782
Sirros <121970263@qq.com>
2021-03-19 05:22:30 +0800

    feat: add tree comp

--- frontend/src/views/Content.vue
+++ frontend/src/views/Content.vue
@@ -6,7 +6,9 @@
           <el-card shadow="always"> {{ `Welcome [${user}]` }} </el-card>
         </el-col>
       </el-row>
-      <div class="leftBottom"></div>
+      <div class="leftBottom">
+        <el-tree :data="data" :props="defaultProps" @node-click="handleNodeClick"></el-tree>
+      </div>
     </div>
     <div class="rightArea">
       <div class="rightTop">
@@ -44,7 +46,51 @@
         style: {
           borderRadius: "50%",
         },
+        data: [{
+          label: '一级 1',
+          children: [{
+            label: '二级 1-1',
+            children: [{
+              label: '三级 1-1-1'
+            }]
+          }]
+        }, {
+          label: '一级 2',
+          children: [{
+            label: '二级 2-1',
+            children: [{
+              label: '三级 2-1-1'
+            }]
+          }, {
+            label: '二级 2-2',
+            children: [{
+              label: '三级 2-2-1'
+            }]
+          }]
+        }, {
+          label: '一级 3',
+          children: [{
+            label: '二级 3-1',
+            children: [{
+              label: '三级 3-1-1'
+            }]
+          }, {
+            label: '二级 3-2',
+            children: [{
+              label: '三级 3-2-1'
+            }]
+          }]
+        }],
+        defaultProps: {
+          children: 'children',
+          label: 'label'
+        }
       };
+    },
+    methods: {
+      handleNodeClick(data) {
+        console.log(data);
+      }
     },
     beforeCreate() {
       const {
