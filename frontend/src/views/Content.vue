e67dd430c591552aa46da6e08dbc6c97f252979b
Sirros <121970263@qq.com>
2021-03-19 04:59:49 +0800

    feat: add file

---
+++ frontend/src/views/Content.vue
@@ -0,0 +1,160 @@
+<template>
+  <div class="container">
+    <div class="leftArea">
+      <el-row class="leftTop">
+        <el-col :span="24">
+          <el-card shadow="always"> {{ `Welcome [${user}]` }} </el-card>
+        </el-col>
+      </el-row>
+      <div class="leftBottom"></div>
+    </div>
+    <div class="rightArea">
+      <div class="rightTop">
+        <el-row :gutter="20">
+          <el-col :span="8">
+            <el-card shadow="hover"> xxxxx </el-card>
+          </el-col>
+          <el-col :span="8">
+            <el-card shadow="hover"> Scan all </el-card>
+          </el-col>
+          <el-col :span="8">
+            <el-card shadow="hover"> Generate Report </el-card>
+          </el-col>
+        </el-row>
+      </div>
+      <div class="rightBottom">
+        <el-row :gutter="20">
+          <el-col :span="12">
+            <div class="content">11111</div>
+          </el-col>
+          <el-col :span="12">
+            <div class="content">222</div>
+          </el-col>
+        </el-row>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        user: window.localStorage.getItem("user"),
+        style: {
+          borderRadius: "50%",
+        },
+      };
+    },
+    beforeCreate() {
+      const {
+        username
+      } = this.$route.params;
+      if (username) {
+        window.localStorage.setItem("user", username);
+      }
+      if (!window.localStorage.getItem("user")) {
+        this.$message({
+          message: "No Login",
+          type: "warning",
+        });
+      }
+    },
+    mounted() {},
+    components: {},
+  };
+</script>
+
+<style>
+  ::-webkit-scrollbar {
+    width: 0 !important;
+  }
+
+  ::-webkit-scrollbar {
+    width: 0 !important;
+    height: 0;
+  }
+
+  .container {
+    display: flex;
+    width: 100%;
+    height: 100%;
+  }
+
+  /* 左边 */
+  .leftArea {
+    width: 20%;
+    height: 100%;
+    padding-top: 10px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .leftTop {
+    text-align: center;
+    height: 10%;
+  }
+
+  .leftArea .leftTop .el-card {
+    font-weight: 500;
+  }
+
+  .leftBottom {
+    width: 100%;
+    flex: 1;
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    box-sizing: border-box;
+  }
+
+  /* 右边 */
+  .rightArea {
+    flex: 1;
+    height: 100%;
+    padding: 10px 20px 0 20px;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .el-row:last-child {
+    margin-bottom: 0;
+  }
+
+  .el-col {
+    border-radius: 4px;
+  }
+
+  .el-button {
+    width: 100%;
+    height: 100%;
+  }
+
+  .rightArea .rightTop {
+    width: 100%;
+    height: 10%;
+  }
+
+  .rightArea .rightBottom {
+    width: 100%;
+    flex: 1;
+  }
+  .rightArea .rightBottom .el-row {
+    height: 100%;
+  }
+  .rightArea .rightBottom .el-row .el-col {
+    height: 100%;
+  }
+  .rightBottom .el-col .content {
+    border: 1px solid rgba(0, 0, 0, 0.25);
+    height: 100%;
+  }
+
+  .rightArea .rightTop .el-card {
+    border-radius: 40px;
+    text-align: center;
+    font-weight: 700;
+  }
+
+  .rightArea .rightTop .el-card:hover {
+    cursor: pointer;
+  }
+</style>
---
+++ frontend/src/views/Login.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="wrapper">
+    <el-row type="flex" class="row-bg logo" justify="space-around">
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light">
+          <el-image :src="src" ></el-image>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light">
+          <el-input v-model="username" clearable placeholder="Input Username"></el-input>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :span="6">
+        <div class="grid-content bg-purple-light">
+          <el-input type="password" v-model="password" clearable placeholder="Input Password"></el-input>
+        </div>
+      </el-col>
+    </el-row>
+    <el-row type="flex" class="row-bg" justify="space-around">
+      <el-col :span="6">
+        <el-button @click="login">Login</el-button>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      src: "https://avatars0.githubusercontent.com/u/74776571?s=100&v=4",
+      username: "",
+      password: "",
+    };
+  },
+  methods: {
+    login() {
+      const { username, password } = this;
+      if(username === "root" && password === "123") {
+        this.$message({
+          message: "Login Success",
+          type: "success"
+        });
+        this.$router.push({ name: "Content", params: { username } })
+      } else if(!username || !password) {
+        this.$message({
+          message: "The UserName or Password cannot be empty...",
+          type: "warning"
+        });
+      } else {
+        this.$message.error("The UserName or Password is incorrect. Please try again");
+      }
+    },
+  },
+  components: {},
+};
+</script>
+
+<style>
+.wrapper {
+  position: absolute;
+  text-align: center;
+  width: 100%;
+  top: 40%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.logo {
+  margin-bottom: 10px;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.row-bg {
+  padding: 10px 0;
+}
+.el-input input {
+  text-align: center;
+}
+.el-button {
+  width: 60%;
+}
+</style>
